467e2ffc5e7e291693124eb6a7c4ace051991685
heskipranata <heskipranta14@gmail.com>
2025-06-08 01:04:11 +0800

    fix : online bootsrap

--- resources/views/beasiswa/index.blade.php
+++ resources/views/beasiswa/index.blade.php
@@ -10,8 +10,15 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 
-    <!-- Vite (Bootstrap & Custom Styles) -->
-    @vite(['resources/css/app.css', 'resources/css/style.css', 'resources/js/app.js', 'resource/js/script.js'])
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3Bpi8L/JZlVg1Z8hY3ZCk+ktKkKP35B" crossorigin="anonymous">
+
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.0/dist/cdn.min.js" defer></script>
+
+    <!-- Bootstrap 5 JS Bundle CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+67FQp14z1H9QOgiPN9MuFQ92vldH" crossorigin="anonymous" defer>
+    </script>
 
     <style>
         body {
@@ -71,7 +78,7 @@
                                             'D3' => 'bg-success text-dark',
                                             'Sarjana-1' => 'bg-warning ',
                                             'Magister' => 'bg-danger text-dark',
-                                            'S2' => 'bg-danger', 
+                                            'S2' => 'bg-danger',
                                         ];
                                     @endphp
 
@@ -117,7 +124,7 @@
 
                     cards.forEach(card => {
                         const cardCategories = card.getAttribute('data-category').split(
-                        ' ');
+                            ' ');
 
                         if (category === 'all' || cardCategories.includes(category)) {
                             card.style.display = 'block';
