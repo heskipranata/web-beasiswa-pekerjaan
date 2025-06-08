f5986b77ddbf79efedd27ab88c035ee7198845d1
heskipranata <heskipranta14@gmail.com>
2025-06-08 15:45:55 +0800

    add: favicon

--- resources/views/components/admin-layout.blade.php
+++ resources/views/components/admin-layout.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
         rel="stylesheet" />
-    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+
 
     <style>
         body {
